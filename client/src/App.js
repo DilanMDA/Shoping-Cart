657cb3a246a2b755b266565b6061d038f9734faa
DilanMDA <akiladilan04@gmail.com>
2021-02-13 03:40:59 +0530

    Header MenuBar changed & eventHandler set

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 import Header from './components/navbar/Header';
+import react from 'react';
 
 const App = () => {
 	return (
 		<>
-		<Header/>
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/login" component={Login} />
-			<Route exact path="/register" component={Register} />
-		</Switch>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/register" component={Register} />
+			</Switch> 	 
 		</>
 	);
 };
--- client/src/components/navbar/Header.js
+++ client/src/components/navbar/Header.js
@@ -1,29 +1,45 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
-import {
+import
+{
 	AppstoreOutlined,
-	MailOutlined,
 	SettingOutlined,
+	UserOutlined,
+	UserAddOutlined,
 } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 
-const Header = () => {
-	const [current, setCurrent] = useState('');
+const Header = () =>
+{
+	const [ current, setCurrent ] = useState( 'home' );
 
-	const handleClick = () => {};
+	const handleClick = ( e ) =>
+	{
+		// console.log(e.key);
+		setCurrent( e.key );
+	};
 
 	return (
-		<Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-			<Menu.Item key="mail" icon={<MailOutlined />}>
+		<Menu onClick={ handleClick } selectedKeys={ [ current ] } mode="horizontal">
+			<Menu.Item key="home" icon={ <AppstoreOutlined /> } className="float-start">
 				Home
 			</Menu.Item>
 
-			<SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
+			<Menu.Item key="register" icon={ <UserAddOutlined /> } className="float-end">
+				Register
+			</Menu.Item>
+			<Menu.Item key="login" icon={ <UserOutlined /> } className="float-end" >
+				Login
+			</Menu.Item>
+
+
+			<SubMenu icon={ <SettingOutlined /> } title="Username">
 				<Menu.Item key="setting:1">Option 1</Menu.Item>
 				<Menu.Item key="setting:2">Option 2</Menu.Item>
 			</SubMenu>
 		</Menu>
+		
 	);
 };
 
