147a9cbc918046af2ef79a15fc8e3db5ca640b83
DilanMDA <akiladilan04@gmail.com>
2021-02-14 23:34:16 +0530

    Register page UI designed

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,7 +1,6 @@
-// import { Form } from 'formik/dist/Form';
 import React from 'react';
 import { useFormik } from 'formik';
-// import * as Yup from 'yup';
+
 import '../css/pages.css';
 
 const Register = () => {
@@ -10,54 +9,105 @@ const Register = () => {
 	//  Validation and error messages
 	const formik = useFormik({
 		initialValues: {
-			fname: 'Akila',
-			lname: '',
-			uname: '',
+			Fname: 'Akila',
+			Lname: '',
+			Uname: '',
 			email: '',
+			password: '',
 		},
 	});
 	console.log('Form values', formik.values);
 
 	return (
-		<form>
-			<label htmlFor="fname"> First Name</label>
-			<input
-				type="text"
-				id="fname"
-				name="fname"
-				onChange={formik.handleChange}
-				value={formik.values.fname}
-			/>
+		<main>
+			<div className="form">
+				<form>
+					<h3>Sign Up</h3>
 
-			<label htmlFor="lname">Name</label>
-			<input
-				type="text"
-				id="lname"
-				name="lname"
-				onChange={formik.handleChange}
-				value={formik.values.lname}
-			/>
+					<div className="form-group">
+						<div className="input">
+							<label>First name</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="First name"
+								id="Fname"
+								name="Fname"
+								onChange={formik.handleChange}
+								value={formik.values.Fname}
+							/>
+						</div>
+					</div>
 
-			<label htmlFor="uname">User Name</label>
-			<input
-				type="text"
-				id="uname"
-				name="uname"
-				onChange={formik.handleChange}
-				value={formik.values.uname}
-			/>
+					<div className="form-group">
+						<div className="input">
+							<label>Last name</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Last name"
+								id="Lname"
+								name="Lname"
+								onChange={formik.handleChange}
+								value={formik.values.Lname}
+							/>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="input">
+							<label>User name</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="User name"
+								id="Uname"
+								name="Uname"
+								onChange={formik.handleChange}
+								value={formik.values.Uname}
+							/>
+						</div>
+					</div>
 
-			<label htmlFor="email">Email</label>
-			<input
-				type="text"
-				id="email"
-				name="email"
-				onChange={formik.handleChange}
-				value={formik.values.email}
-			/>
+					<div className="form-group">
+						<div className="input">
+							<label>Email address</label>
+							<input
+								type="email"
+								className="form-control"
+								placeholder="Enter email"
+								id="email"
+								name="email"
+								onChange={formik.handleChange}
+								value={formik.values.email}
+							/>
+						</div>
+					</div>
 
-			<button>Submit</button>
-		</form>
+					<div className="form-group">
+						<div className="input">
+							<label>Password</label>
+							<input
+								type="password"
+								className="form-control"
+								placeholder="Enter password"
+								id="password"
+								name="password"
+								onChange={formik.handleChange}
+								value={formik.values.password}
+							/>
+						</div>
+					</div>
+					<div className="btn">
+						<button type="submit" className="btn btn-primary btn-block">
+							Sign Up
+						</button>
+					</div>
+					<p className="forgot-password text-right">
+						Already registered <a href="#">sign in?</a>
+					</p>
+				</form>
+			</div>
+		</main>
 	);
 };
 
