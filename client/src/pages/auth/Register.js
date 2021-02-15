7b4a6e8cf94fdd6361953d1d3ce75a97a0ef5b27
DilanMDA <akiladilan04@gmail.com>
2021-02-16 01:05:40 +0530

    Form Components with Formik

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
-
 import '../css/pages.css';
 
 const initialValues = {
@@ -12,7 +11,7 @@ const initialValues = {
 	password: '',
 };
 const onSubmit = (values) => {
-	// console.log('Form data', values);
+	console.log('Form data', values);
 };
 
 const validationSchema = yup.object().shape({
@@ -31,112 +30,96 @@ const Register = () => {
 	// Managing the form state
 	// Handling form submission
 	//  Validation and error messages
-	const formik = useFormik({
-		initialValues,
-		onSubmit,
-		validationSchema,
-	});
-	console.log('Visted Field', formik.touched);
 
 	return (
 		<main>
-			<div className="form">
-				<form onSubmit={formik.handleSubmit}>
-					<h3>Sign Up</h3>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={onSubmit}>
+				<div className="form">
+					<Form>
+						<h3>Sign Up</h3>
 
-					<div className="form-group">
-						<div className="input">
-							<label className="label">First name</label>
-							<input
-								type="text"
-								className="form-control"
-								placeholder="First name"
-								id="Fname"
-								name="Fname"
-								{...formik.getFieldProps('Fname')}
-							/>
-							{formik.touched.Fname && formik.errors.Fname ? (
-								<div className="error">{formik.errors.Fname}</div>
-							) : null}
+						<div className="form-group">
+							<div className="input">
+								<label className="label">First name</label>
+								<Field
+									type="text"
+									className="form-control"
+									placeholder="First name"
+									id="Fname"
+									name="Fname"
+								/>
+								<ErrorMessage className="error" name="Fname" />
+							</div>
 						</div>
-					</div>
 
-					<div className="form-group">
-						<div className="input">
-							<label>Last name</label>
-							<input
-								type="text"
-								className="form-control"
-								placeholder="Last name"
-								id="Lname"
-								name="Lname"
-								{...formik.getFieldProps('Lname')}
-							/>
-							{formik.touched.Lname && formik.errors.Lname ? (
-								<div className="error">{formik.errors.Lname}</div>
-							) : null}
+						<div className="form-group">
+							<div className="input">
+								<label>Last name</label>
+								<Field
+									type="text"
+									className="form-control"
+									placeholder="Last name"
+									id="Lname"
+									name="Lname"
+								/>
+								<ErrorMessage className="error" name="Lname" />
+							</div>
 						</div>
-					</div>
-					<div className="form-group">
-						<div className="input">
-							<label>User name</label>
-							<input
-								type="text"
-								className="form-control"
-								placeholder="User name"
-								id="Uname"
-								name="Uname"
-								{...formik.getFieldProps('Uname')}
-							/>
-							{formik.touched.Uname && formik.errors.Uname ? (
-								<div className="error">{formik.errors.Uname}</div>
-							) : null}
+						<div className="form-group">
+							<div className="input">
+								<label>User name</label>
+								<Field
+									type="text"
+									className="form-control"
+									placeholder="User name"
+									id="Uname"
+									name="Uname"
+								/>
+								<ErrorMessage className="error" name="Uname" />
+							</div>
 						</div>
-					</div>
 
-					<div className="form-group">
-						<div className="input">
-							<label>Email address</label>
-							<input
-								type="email"
-								className="form-control"
-								placeholder="Enter email"
-								id="email"
-								name="email"
-								{...formik.getFieldProps('email')}
-							/>
-							{formik.touched.email && formik.errors.email ? (
-								<div className="error">{formik.errors.email}</div>
-							) : null}
+						<div className="form-group">
+							<div className="input">
+								<label>Email address</label>
+								<Field
+									type="email"
+									className="form-control"
+									placeholder="Enter email"
+									id="email"
+									name="email"
+								/>
+								<ErrorMessage className="error" name="email" />
+							</div>
 						</div>
-					</div>
 
-					<div className="form-group">
-						<div className="input">
-							<label>Password</label>
-							<input
-								type="password"
-								className="form-control"
-								placeholder="Enter password"
-								id="password"
-								name="password"
-								{...formik.getFieldProps('password')}
-							/>
-							{formik.touched.password && formik.errors.password ? (
-								<div className="error">{formik.error.password}</div>
-							) : null}
+						<div className="form-group">
+							<div className="input">
+								<label>Password</label>
+								<Field
+									type="password"
+									className="form-control"
+									placeholder="Enter password"
+									id="password"
+									name="password"
+								/>
+								<ErrorMessage className="error" name="password" />
+							</div>
 						</div>
-					</div>
 
-					<button type="submit" className="btn">
-						Sign Up
-					</button>
+						<button type="submit" className="btn">
+							Sign Up
+						</button>
 
-					<p className="signIn">
-						Already registered <a href="#">sign in?</a>
-					</p>
-				</form>
-			</div>
+						<p className="signIn">
+							Already registered <a href="#">sign in?</a>
+						</p>
+					</Form>
+				</div>
+			</Formik>
 		</main>
 	);
 };
