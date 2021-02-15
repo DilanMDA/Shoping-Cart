0bb5f29fdcae01e97b7f15b0764018dab37a7b5a
DilanMDA <akiladilan04@gmail.com>
2021-02-16 03:48:23 +0530

    Validation Modified(API path setuped)

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -10,6 +10,11 @@ const initialValues = {
 	email: '',
 	password: '',
 };
+
+//API call backend and compare email
+const emailAddresses = ['test@gmail.com', 'test2@gmail.com', 'test3@gmail.com'];
+const UserName = ['Akila', 'Amila'];
+
 const onSubmit = (values) => {
 	console.log('Form data', values);
 };
@@ -17,13 +22,26 @@ const onSubmit = (values) => {
 const validationSchema = yup.object().shape({
 	Fname: yup.string().required('Required'),
 	Lname: yup.string().required('Required'),
-	Uname: yup.string().required('Required'),
-	email: yup.string().email('Invalid email format').required('Required'),
+	Uname: yup
+		.string()
+		.required('Required')
+		.notOneOf(
+			UserName,
+			'All ready have this User name pleas enter another one',
+		),
+	email: yup
+		.string()
+		.email('Invalid email format')
+		.notOneOf(emailAddresses, 'Email already taken!')
+		.required('Required'),
 	password: yup
 		.string()
-		.required('No password provided.')
+		.required('No password provided. pleas enter your password')
 		.min(8, 'Password is too short - should be 8 chars minimum.')
-		.matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+		.matches(
+			'^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$',
+			'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
+		),
 });
 
 const Register = () => {
@@ -48,6 +66,7 @@ const Register = () => {
 									type="text"
 									className="form-control"
 									placeholder="First name"
+									autoComplete="off"
 									id="Fname"
 									name="Fname"
 								/>
@@ -62,6 +81,7 @@ const Register = () => {
 									type="text"
 									className="form-control"
 									placeholder="Last name"
+									autoComplete="off"
 									id="Lname"
 									name="Lname"
 								/>
@@ -75,6 +95,7 @@ const Register = () => {
 									type="text"
 									className="form-control"
 									placeholder="User name"
+									autoComplete="off"
 									id="Uname"
 									name="Uname"
 								/>
@@ -89,6 +110,7 @@ const Register = () => {
 									type="email"
 									className="form-control"
 									placeholder="Enter email"
+									autoComplete="off"
 									id="email"
 									name="email"
 								/>
@@ -103,6 +125,7 @@ const Register = () => {
 									type="password"
 									className="form-control"
 									placeholder="Enter password"
+									autoComplete="off"
 									id="password"
 									name="password"
 								/>
@@ -110,13 +133,18 @@ const Register = () => {
 							</div>
 						</div>
 
-						<button type="submit" className="btn">
-							Sign Up
-						</button>
+						<div className="btn">
+							<button type="submit" className="btnSubmit">
+								Sign Up
+							</button>
+							<button type="reset" className="btnReset">
+								Reset
+							</button>
+						</div>
 
-						<p className="signIn">
+						{/* <p className="signIn">
 							Already registered <a href="#">sign in?</a>
-						</p>
+						</p> */}
 					</Form>
 				</div>
 			</Formik>
